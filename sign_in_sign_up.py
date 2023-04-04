08147c3e45b4ea86f1ad26d876885909215e6d31
Eytan Benittah <75117352+Eytan425@users.noreply.github.com>
2023-04-04 19:31:31 +0300

    Add files via upload

--- sign_in_sign_up.py
+++ sign_in_sign_up.py
@@ -1,5 +1,6 @@
 import os
 import json
+tries = 0
 
 # Load existing data from the JSON file if it exists and is not empty
 if os.path.exists("users.json") and os.stat("users.json").st_size != 0:
@@ -18,20 +19,13 @@ def is_username_available(username):
 # Get input from the user
 user_type = input("Are you a new user? (y/n): ")
 
-if user_type == "n":
-    while True:
-        username = input("Enter your username: ")
-        password = input("Enter your password: ")
 
-        if username in data and data[username]["password"] == password:
-            print("Welcome back, {}! You have {} coins".format(username, data[username]["coins"]))
-            break
-        else:
-            print("Username or password incorrect, please try again.")
+                
 
-elif user_type == "y":
+if user_type == "y":
     while True:
         username = input("Enter a username: ")
+        email = input("Enter your email: ")
         if not is_username_available(username):
             print("The username is already taken. Please enter a new username.")
             continue
@@ -45,16 +39,44 @@ elif user_type == "y":
             break
         else:
             print("Passwords do not match, please try again.")
+    
+    security = input("What is your grandmas name? ")
 
     coins = 100
-    data[username] = {"password": password, "coins": coins}
+    data[username] = {"Password": password, "Coins": coins, "Security Question": security, "Email": email}
 
     with open("users.json", "w") as f:
         json.dump(data, f)
     print("Congratulations! Your account has been created with {} coins".format(coins))
 
+
+
+elif user_type == "n":
+    while True:
+        username = input("Enter your username: ")
+        password = input("Enter your password: ")
+
+        if username in data and data[username]["Password"] == password:
+            print("Welcome back, {}! You have {} coins".format(username, data[username]["Coins"]))
+            break
+        else:
+            print("Username or password incorrect, please try again.")
+            tries += 1
+            
+            if (tries == 5):
+                forget = input("Have you forgotten your password? (y/n): ")
+                if(forget.lower() == "y"):
+                    security1 = input("What is your grandmas name? ")
+                        
+                    if username in data and data[username]["Security Question"] == security1:
+                        print("Welcome back, {}! You have {} coins".format(username, data[username]["Coins"]))
+                        break
+                    else:
+                        print("Wrong, restart the game and sign in again.")
+                        break
 else:
-    print("Invalid input. Please enter 'y' or 'n'.")
+    print("Invalid input. Please enter 'y' or 'n'.")                        
+
 
 def find_coins_for_username(username):
     # Load the current data from the JSON file
@@ -63,7 +85,7 @@ def find_coins_for_username(username):
 
     # Find the user and return their coin balance
     if username in data:
-        return data[username]["coins"]
+        return data[username]["Coins"]
     else:
         return None
 
@@ -75,7 +97,7 @@ def update_coins_for_username(user):
     # Find the user and update their coin balance
     for username in data:
         if username == user["user"]:
-            data[username]["coins"] = user["coins"]
+            data[username]["Coins"] = user["Coins"]
             break
 
     # Write the updated data back to the JSON file
